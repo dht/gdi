a71dedc6fd577422cd150240ee888085c58e0ece
dht <dht@users.noreply.github.com>
2022-11-29 20:19:41 +0200

    feat(mixer): bootstrap saga closes #191

--- packages/gdi-web-ui/src/index.ts
+++ packages/gdi-web-ui/src/index.ts
@@ -27,6 +27,7 @@ export { TypeView } from './components/TypeView/TypeView';
 export { Weather } from './components/Weather/Weather';
 export { WindowHeader } from './components/WindowHeader/WindowHeader';
 export { siteDefaults } from './definitions/site.defaults';
+import { useCrudDefinitions } from '@gdi/platformer';
 
 export {
     Accordion,
