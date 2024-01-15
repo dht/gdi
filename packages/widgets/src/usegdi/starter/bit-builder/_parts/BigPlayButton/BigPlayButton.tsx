82e4266b6b03b077a392179dd5b2ed15de44b433
dht <137832+dht@users.noreply.github.com>
2024-01-15 15:00:51 +0200

    fix(BitBuilder): first animation is not playing on mobile (#15)

--- packages/widgets/src/usegdi/starter/bit-builder/_parts/BigPlayButton/BigPlayButton.tsx
+++ packages/widgets/src/usegdi/starter/bit-builder/_parts/BigPlayButton/BigPlayButton.tsx
@@ -1,20 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Wrapper } from './BigPlayButton.style';
-import { Icon, isMobile } from '@gdi/ui';
-import { invokeEvent } from 'shared-base';
+import { Icon, Loader, isMobile } from '@gdi/ui';
 
 export type BigPlayButtonProps = {
   onPlay: () => void;
 };
 
 export function BigPlayButton(props: BigPlayButtonProps) {
+  const [canPlay, setCanPlay] = useState(false);
+
+  useEffect(() => {
+    const delay = isMobile() ? 2000 : 800;
+
+    const timeout = setTimeout(() => {
+      setCanPlay(true);
+    }, delay);
+
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, []);
+
+  function onPlay() {
+    if (!canPlay) return;
+
+    props.onPlay();
+  }
+
+  function renderLoader() {
+    return <Loader color='green' size={30} />;
+  }
+
+  function renderInner() {
+    if (!canPlay) {
+      return renderLoader();
+    }
+
+    return <Icon name='play_arrow' size={isMobile() ? 40 : 50} color='#aca' />;
+  }
+
   return (
-    <Wrapper
-      className='BigPlayButton-wrapper'
-      data-testid='BigPlayButton-wrapper'
-      onClick={props.onPlay}
-    >
-      <Icon name='play_arrow' size={isMobile() ? 40 : 50} color='#aca' />
+    <Wrapper className='BigPlayButton-wrapper' data-testid='BigPlayButton-wrapper' onClick={onPlay}>
+      {renderInner()}
     </Wrapper>
   );
 }
